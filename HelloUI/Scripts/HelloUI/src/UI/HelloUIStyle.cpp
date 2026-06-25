cd5164ad6bbdc2ab72b9ef26465cf3838c5f0d4a
Andreichev Michail <andreichev.m@mail.ru>
2026-06-25 07:08:41 +0300

    Hello UI update

--- HelloUI/Scripts/HelloUI/src/UI/HelloUIStyle.cpp
+++ HelloUI/Scripts/HelloUI/src/UI/HelloUIStyle.cpp
@@ -15,10 +15,6 @@ std::shared_ptr<PandaUI::Label> makeLabel(std::string text, float fontSize, Pand
 std::shared_ptr<PandaUI::Button> makeButton(std::string title, PandaUI::Color color) {
     auto button = std::make_shared<PandaUI::Button>(std::move(title));
     button->layout().setWidth(PandaUI::Length::points(168.f));
-    button->setNormalColor(color);
-    button->setHoveredColor(PandaUI::Color(0x4A92FFFF));
-    button->setPressedColor(PandaUI::Color(0x1C58BDFF));
-    button->setTextColor(PandaUI::Color(0xFFFFFFFF));
     return button;
 }
 
